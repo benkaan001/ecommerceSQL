8201f128b0a9cf27ded17ec30a2e97c36e391631
Ben Kaan <benkaan001@gmail.com>
2021-10-31 22:28:28 -0500

    add/define tag columns

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,17 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    //define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define tag_name column
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
