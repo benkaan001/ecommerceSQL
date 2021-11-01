41e49a767be2faf2e42a72e13e7f81a656ffb9f1
Ben Kaan <benkaan001@gmail.com>
2021-10-31 22:13:10 -0500

    dadd/define Cateogry columns

--- models/Category.js
+++ models/Category.js
@@ -7,12 +7,30 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    //define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //define a category_name column
+    catgory_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
   },
   {
+     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
+    // don't pluralize name of database table
     freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
     underscored: true,
+     // make it so our model name stays lowercase in the database
     modelName: 'category',
   }
 );
