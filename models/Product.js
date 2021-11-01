7b9dc6a3e581c0b6ce3ec556776242ea21cb4da0
Ben Kaan <benkaan001@gmail.com>
2021-10-31 22:26:21 -0500

    add/define product columns

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    // define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define a product_name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // define a price column
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: true
+    },
+    // define a stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: true
+    },
+    //define a category_id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      references : {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
