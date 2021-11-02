c4c5898beb37733a5a2673b2ecdd37224db6cb34
Ben Kaan <benkaan001@gmail.com>
2021-11-01 22:33:21 -0500

    create associations

--- models/index.js
+++ models/index.js
@@ -6,11 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey:'category_id'
+});
+
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey:'product_tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey:'product_tag'
+});
 
 module.exports = {
   Product,
